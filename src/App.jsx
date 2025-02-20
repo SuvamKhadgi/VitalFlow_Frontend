f8daf06fa3c9aacfe66cfd98e569099fedc9a369
SuvamKhadgi <appleb986@gmail.com>
2025-02-20 23:43:24 +0545

    mafde some new pages

---
+++ src/App.jsx
@@ -0,0 +1,102 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { lazy, Suspense } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Additems from "./core/private/all items/add_items";
+import Allitems from "./core/private/all items/allitems";
+import Allorder from "./core/private/all order/allorder";
+import Users from "./core/private/all users/allusers";
+import Dashboard from "./core/private/dashboard/dashboard";
+import BabyCare from "./core/public/product/babycare";
+const Home = lazy(() => import("./core/public/home/home"));
+const Login = lazy(() => import("./core/public/creads/login_page"));
+const Signup = lazy(() => import("./core/public/creads/signup_page"));
+const queryClient = new QueryClient();
+function App() {
+    const publicRoutes = [
+        {
+            path: "/login",
+            element: (
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Login />
+                </Suspense>
+            ),
+        },
+        {
+            path: "/signup",
+            element: (
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Signup />
+                </Suspense>
+            ),
+        },
+        {
+            path: "/",
+            element: (
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Home />
+                </Suspense>
+            ),
+        },
+        {
+            path: "/admindashboard",
+            element: (
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Dashboard />
+                </Suspense>
+            ),
+        },
+        {
+            path: "/additems",
+            element: (
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Additems />
+                </Suspense>
+            ),
+        },
+        {
+            path: "/getitems",
+            element: (
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Allitems />
+                </Suspense>
+            ),
+        },
+        {
+            path: "/allusers",
+            element: (
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Users />
+                </Suspense>
+            ),
+        },
+        {
+            path: "/allorder",
+            element: (
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Allorder />
+                </Suspense>
+            ),
+        },
+        {
+            path: "/babycare",
+            element: (
+                <Suspense fallback={<div>Loading...</div>}>
+                   <BabyCare/>
+                </Suspense>
+            ),
+        },
+    ];
+
+    // const privateRoutes=[];
+    const router = createBrowserRouter(publicRoutes);
+
+    return (
+        <>
+            <QueryClientProvider client={queryClient}>
+                <RouterProvider router={router} />
+            </QueryClientProvider>
+        </>
+    );
+}
+
+export default App;
